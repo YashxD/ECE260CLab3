906bd0eb6b99ca80f6c5a0c5176b9a009f9fffa2
Yash <yashxd01@gmail.com>
2024-05-19 21:08:31 -0700

    2d-blur-filter algo + framework cleanup

--- 2d-blur-filter/src/filter.cu
+++ 2d-blur-filter/src/filter.cu
@@ -5,10 +5,29 @@
 
 using namespace std;
 
+#define FILTER_SIZE 3
+#define FILTER_LIM FILTER_SIZE/2
+
 __global__
 void kernel_filter(const uchar * input, uchar * output, const uint height, const uint width)
 {
 	// TODO: Implement a blur filter for the camera (averaging an NxN array of pixels
+
+  // uint8_t filterSize = 3;
+  uint16_t kernelSum = 0;
+
+  for (int i = FILTER_LIM; i < height + FILTER_LIM; i++) {
+    for (int j = FILTER_LIM; j < width + FILTER_LIM; j++) {
+      kernelSum = 0;
+      // Add all the elements to get the sum
+      for (int ii = i - FILTER_LIM; ii < i + FILTER_LIM; ii++) {
+        for (int jj = j - FILTER_LIM; jj < j + FILTER_LIM; jj++) {
+          kernelSum += input[(ii * width) + jj];
+        }
+      }
+      output[(i * width) + j] = kernelSum/(FILTER_SIZE * FILTER_SIZE);
+    }
+  }
 }
 
 inline int divup(int a, int b)
